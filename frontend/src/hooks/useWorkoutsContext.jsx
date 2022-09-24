a8decee8e5f403165712c222bc3038a649661f93
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2022-09-24 08:02:28 -0700

    Add files via upload
    mern-stack-auth FE

---
+++ frontend/src/hooks/useWorkoutsContext.jsx
@@ -0,0 +1,10 @@
+import { WorkoutContext } from '../context/WorkoutContext'
+import { useContext } from 'react'
+
+export const useWorkoutsContext = () => {
+    const context = useContext(WorkoutContext)
+    if (!context) {
+        throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
+    }
+    return context
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { WorkoutContextProvider } from './context/WorkoutContext'
+import { AuthContextProvider  } from './context/authContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthContextProvider >
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContextProvider >
+  </React.StrictMode>
+);
